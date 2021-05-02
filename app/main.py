2426248e8d55a59a49d5e0a8de8744bd9dc29536
takumiw <w.tak.1229@gmail.com>
2021-05-02 16:59:48 +0900

    feat: add path /countup/round{round}

--- app/main.py
+++ app/main.py
@@ -7,3 +7,7 @@ app = FastAPI()
 @app.get('/')
 async def hello():
     return {"text": "hello world!"}
+
+@app.get("/countup/round{round}")
+async def render_countup_round(round):
+    return {"round": round}
