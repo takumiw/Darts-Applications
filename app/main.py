23b89cd7b9fdd75b8552df3544ce4414df8eccfd
takumiw <w.tak.1229@gmail.com>
2021-05-03 16:33:31 +0900

    feat: countup game

--- app/main.py
+++ app/main.py
@@ -1,13 +1,32 @@
-from fastapi import FastAPI
+import pathlib
 
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get('/')
-async def hello():
-    return {"text": "hello world!"}
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/countup/round{round}")
-async def render_countup_round(round):
-    return {"round": round}
+
+@app.post("/countup", response_class=HTMLResponse)
+async def render_countup(
+    request: Request, _round: int = Form(...), total_score: int = Form(0), score: int = Form(0)
+):
+    _round += 1
+    total_score += score
+    if _round <= 8:
+        return templates.TemplateResponse(
+            "countup.html", {"request": request, "_round": _round, "total_score": total_score}
+        )
+    else:
+        avg = round(total_score / 8)
+        return templates.TemplateResponse(
+            "result.html", {"request": request, "total_score": total_score, "avg": avg}
+        )
